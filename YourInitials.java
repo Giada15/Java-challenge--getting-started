3f5b4d563ce5284255022a0b73a70e364ddbc58e
Giada15 <tascheriogiada@gmail.com>
2024-03-27 10:34:23 -0400

    first commit

---
+++ YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         System.out.println(" GGG   TTTTT");
+         System.out.println("G   G    T");
+         System.out.println("G        T");
+         System.out.println("G GGG    T");
+         System.out.println("G   G    T");
+         System.out.println("G   G    T");
+         System.out.println(" GGG     T");
+
+    }
+}
